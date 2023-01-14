84f304e5588c494221e4441c5208a34b9fee305f
Ivan Li <ivanli2048@gmail.com>
2023-01-14 16:21:56 +0800

    feat: proxy support.

---
+++ src/deps.ts
@@ -0,0 +1,2 @@
+export * as sdk from 'https://deno.land/x/appwrite@6.2.0/mod.ts';
+export * as httpServer from "https://deno.land/std@0.172.0/http/server.ts";
---
+++ src/main.ts
@@ -0,0 +1,90 @@
+import { httpServer, sdk } from "./deps.ts";
+
+const patternText =
+  Deno.env.get("CALLBACK_PROXY_FOR_APPWRITE_FUNCTIONS_URL_PATTERN") ??
+    "/v1/webhook-proxy/:projectId/:functionId";
+const pattern = new URLPattern({
+  pathname: patternText,
+});
+
+async function handler(req: Request): Promise<Response> {
+  const url = new URL(req.url);
+  const match = pattern.exec(url);
+  if (!match) {
+    return new Response(
+      `not match pattern: "${patternText}". your url path: "${url.pathname}"`,
+      {
+        status: 404,
+      },
+    );
+  }
+  const endpoint = req.headers.get('x-appwrite-endpoint') ?? url.searchParams.get("endpoint") ??
+    Deno.env.get("CALLBACK_PROXY_FOR_APPWRITE_FUNCTIONS_ENDPOINT");
+  const apiKey = req.headers.get('x-appwrite-api-key') ?? url.searchParams.get("apiKey") ??
+    Deno.env.get("CALLBACK_PROXY_FOR_APPWRITE_FUNCTIONS_API_KEY");
+  const projectId = match.pathname.groups.projectId ??
+    Deno.env.get("CALLBACK_PROXY_FOR_APPWRITE_FUNCTIONS_PROJECT_ID");
+  const functionId = match.pathname.groups.functionId ??
+    Deno.env.get("CALLBACK_PROXY_FOR_APPWRITE_FUNCTIONS_FUNCTION_ID");
+
+  if (!endpoint) {
+    return new Response(`missing endpoint!`, {
+      status: 400,
+    });
+  }
+  if (!apiKey) {
+    return new Response(`missing apiKey!`, {
+      status: 400,
+    });
+  }
+  if (!projectId) {
+    return new Response(`missing projectId!`, {
+      status: 400,
+    });
+  }
+  if (!functionId) {
+    return new Response(`missing functionId!`, {
+      status: 400,
+    });
+  }
+
+  const client = new sdk.Client().setEndpoint(endpoint).setKey(apiKey).setProject(projectId)
+    .setProject(projectId);
+  const functions = new sdk.Functions(client);
+  const getBody = async () => {
+    const isJSON = req.headers.get("content-type") === "application/json";
+    const { value } =
+      await req.body?.pipeThrough(new TextDecoderStream()).getReader().read() ??
+        {};
+
+    if (isJSON && value) {
+      try {
+        return JSON.parse(value);
+      } catch (error) {
+        console.error(`can not parse body as json. ${error}`);
+      }
+    } else {
+      return value;
+    }
+  };
+  const data = {
+    method: req.method,
+    body: await getBody(),
+    params: Object.fromEntries(Array.from(url.searchParams.keys()).map(name => [name, ((arr) => (arr.length > 1 ? arr : arr[0]))(url.searchParams.getAll(name))])),
+    headers: Object.fromEntries(req.headers.entries()),
+  }
+  const execution = await functions.createExecution(functionId, JSON.stringify(data));
+  const headers = new Headers();
+  if (execution.status === 'failed') {
+    return new Response(execution.response ?? 'Failed.', {
+      status: 500,
+      headers: headers,
+    });
+  }
+  return new Response(execution.response, {
+    status: execution.statusCode,
+  });
+}
+httpServer.serve(handler, {
+  port: +(Deno.env.get('CALLBACK_PROXY_FOR_APPWRITE_FUNCTIONS_URL_PORT') || '4444'),
+});
